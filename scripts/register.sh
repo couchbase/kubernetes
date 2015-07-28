ef320d8548160b735e62918389341dcfc582f032
Patrick Heneise <patrick@fastmail.im>
2015-07-28 18:36:10 +0200

    Update register.sh
    GCE beta v1 adjustments

--- scripts/register.sh
+++ scripts/register.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-PROJECT_ID=[your project id]
+PROJECT_ID=erudite-river-101207
 ZONE=us-central1-f
 REGION=us-central1
-CLUSTER=[your cluster name]
+CLUSTER=socius-data
 
-CBUSER=user
+CBUSER=Administrator
 CBPASSWORD=passw0rd
 
 # config
-gcloud components update alpha
+gcloud components update beta
 gcloud config set project $PROJECT_ID
 gcloud config set compute/zone $ZONE
 gcloud config set compute/region $REGION
 
-gcloud alpha container clusters create $CLUSTER \
+gcloud beta container clusters create $CLUSTER \
     --num-nodes 2 \
     --quiet \
     --machine-type g1-small
 
-kubectl config use-context gke_erudite-pride-94911_us-central1-f_$CLUSTER
 gcloud config set container/cluster $CLUSTER
 
 
 # pods
 printf "\nCreating pods ...\n"
+kubectl create -f services/app-etcd.json
 kubectl create -f pods/app-etcd.json
 
 printf "\nWaiting for cluster to initialise ...\n"
-sleep 200
+sleep 300
 
 # config file adjustments
 printf "\nAdjusting config files ..."
 PODIP=$(kubectl get -o json pod app-etcd | jsawk 'return this.status.podIP')
 sleep 5
-ETCDHOST=$(kubectl get -o json pod app-etcd | jsawk 'return this.spec.host')
+ETCDHOST=$(kubectl get -o json pod app-etcd | jsawk 'return this.spec.nodeName')
 
 sed -i'.bak' "s/etcd.pod.ip/$PODIP/" replication-controllers/couchbase.controller.json
 
@@ -43,15 +43,15 @@ gcloud --quiet compute ssh $ETCDHOST --command "curl --silent -L http://$PODIP:2
 NODE1=$(gcloud compute instances list --format json | ./nodes.js | awk 'NR==1{print $1}')
 NODE2=$(gcloud compute instances list --format json | ./nodes.js | awk 'NR==2{print $1}')
 
-# services
-printf "\nCreating services ...\n"
-kubectl create -f services/couchbase.service.json
-
-
 # replication-controllers
 printf "\nCreating replication-controllers ...\n"
 kubectl create -f replication-controllers/couchbase.controller.json
+kubectl create -f replication-controllers/couchbase-admin.controller.json
 
+# services
+printf "\nCreating services ...\n"
+kubectl create -f services/couchbase.service.json
+kubectl create -f services/couchbase-admin.service.json
 
 # firewall and forwarding-rules
 printf "\nCreating firewall and forwarding-rules ...\n"
@@ -66,6 +66,6 @@ rm replication-controllers/couchbase.controller.json
 mv replication-controllers/couchbase.controller.json.bak replication-controllers/couchbase.controller.json
 
 # Done.
-CBNODEIP=$(gcloud compute instances describe $NODE1 --format json | jsawk 'return this.networkInterfaces[0].accessConfigs[0].natIP')
+CBNODEIP=$(kubectl get -o json service couchbase-admin-service | jsawk 'return this.status.loadBalancer.ingress[0].ip')
 
-printf "\nDone\n\n. Go to http://$CBNODEIP:8091\n\n".
+printf "\nDone.\n\nGo to http://$CBNODEIP:8091\n\n".
